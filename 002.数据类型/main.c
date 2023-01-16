6908d6810b27d33338f9b9c0d87630dbe94eeffe
Easy1229 <2859412930@qq.com>
2023-01-16 17:25:53 +0800

    UPDate

--- 002.数据类型/main.c
+++ 002.数据类型/main.c
@@ -2,8 +2,12 @@
 #include <stdlib.h>
 
 int main() {
+	//int a;
+	unsigned a=-1;
+	printf("%d\n",a);
 	printf("%d\n",sizeof(int));
 	printf("%d\n",sizeof(short));
+	printf("%d\n",sizeof (char));
 	printf("%d\n",sizeof(float));
 	printf("%d\n",sizeof(double));
 	printf("%d\n",sizeof(long));
---
+++ 003.数学运算/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+	int a=3;
+	int b=2;
+	printf("a+b=%d\n",a+b);
+	printf("a-b=%d\n",a-b);
+	printf("a*b=%d\n",a*b);
+	printf("a/b=%f\n",a/b);
+	printf("a%%b=%d\n",a%b);
+	return 0;
+} 
