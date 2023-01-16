a859ca374d1bd943ea030b3147ed5151a236d030
Easy1229 <2859412930@qq.com>
2023-01-17 00:14:48 +0800

    UPDate

--- 003.数学运算/main.c
+++ 003.数学运算/main.c
@@ -4,10 +4,16 @@
 int main(){
 	int a=3;
 	int b=2;
+	float c=2.0f; 
 	printf("a+b=%d\n",a+b);
 	printf("a-b=%d\n",a-b);
 	printf("a*b=%d\n",a*b);
-	printf("a/b=%f\n",a/b);
+	printf("a/c=%f\n",a/c);
 	printf("a%%b=%d\n",a%b);
+	Test(6666);
 	return 0;
 } 
+int Test(int num){
+	printf(num);
+	return num;
+}
